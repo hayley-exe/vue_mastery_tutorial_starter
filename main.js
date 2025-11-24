c9eee46bbd35aac53a9151ea6f36f5626a8b45ee
hayley-exe <526hslater@frhsd.com>
2025-11-24 10:03:37 -0500

    attribute binding

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            image: './assets/images/socks_green.jpg',
 
-            description: 'A warm fuzzy pair of socks.'
+            url: 'https://www.vuemastery.com/'
 
         }
     }
